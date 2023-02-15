8303133a99bfa6f73ab3fbad6b2a908c34df00bd
ambarlam <114983001+ambarlam@users.noreply.github.com>
2023-02-15 20:22:46 +0800

    Initial with sauce labs (#3)
    * Implementation of configurations from property file
    
    * Update sauceLabs credentials configuration
    
    ---------
    
    Co-authored-by: ambarlam <anamarie.barlam@softvision.com>

---
+++ src/main/java/com/utils/ConfigurationReader.java
@@ -0,0 +1,60 @@
+package com.utils;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Properties;
+
+public class ConfigurationReader {
+	private static ConfigurationReader configurations;
+	private Properties properties;
+	
+	public ConfigurationReader() {
+		try(BufferedReader reader = new BufferedReader(new FileReader(Literals.RESOURCES_PATH + "Configuration.properties"));){
+			properties = new Properties();
+			properties.load(reader);
+		} catch (IOException e) {
+			// TODO: handle exception
+		}
+	}
+	
+	public String getEnvironmentToRun() {
+		return properties.getProperty("ENVIRONMENT_TO_RUN");
+	}
+	
+	public String getPlatformName() {
+		return properties.getProperty("PLATFORM_NAME");
+	}
+	
+	public String getPlatformVersion() {
+		return properties.getProperty("PLATFORM_VERSION");
+	}
+	
+	public String getDeviceName() {
+		return properties.getProperty("DEVICE_NAME");
+	}
+	
+	public String getApkFileName() {
+		return properties.getProperty("APK_FILE_NAME");
+	}
+	
+	public String getSauceUsername() {
+		return properties.getProperty("SAUCE_USERNAME");
+	}
+	
+	public String getSauceAccessKey() {
+		return properties.getProperty("SAUCE_ACCESS_KEY");
+	}
+	
+	public String getSauceRegion() {
+		return properties.getProperty("SAUCE_REGION");
+	}
+	
+	public String getSauceReportsName() {
+		return properties.getProperty("SAUCE_REPORTS");
+	}
+	
+	public static ConfigurationReader getConfigurations() {
+		return (configurations == null) ? new ConfigurationReader() : configurations;
+	}
+}
--- src/main/java/com/utils/Literals.java
+++ src/main/java/com/utils/Literals.java
@@ -1,6 +1,9 @@
 package com.utils;
 
 public class Literals {
+	// FILE PATH
+	public static final String RESOURCES_PATH = "src/test/resources/";
+	
 	// User Account Credentials
 	public static final String VALID_USERNAME = "standard_user";
 	public static final String VALID_PASSWORD = "secret_sauce";
@@ -11,4 +14,13 @@ public class Literals {
 	public static final String ERR_USERNAME_REQUIRED = "Username is required";
 	public static final String ERR_PASSWORD_REQUIRED = "Password is required";
 	public static final String ERR_CREDENTIALS_DONT_MATCH = "Username and password do not match any user in this service.";
+	
+	// Environments to run
+	public static final String SAUCELABS = "SAUCELABS";
+	public static final String LOCAL = "LOCAL";
+	
+	// App configuration constants
+	public static String APPIUM_SERVER = "http://localhost:4723/wd/hub";
+	public static String appWaitActivity = "com.swaglabsmobileapp.MainActivity";
+	public static String automationName = "UiAutomator2";
 }
